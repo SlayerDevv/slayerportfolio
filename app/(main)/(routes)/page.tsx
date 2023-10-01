e0aa9b088796a19fb22dcf30b8992ff14f5f6c55
slayer <hdfhoussam18@gmail.com>
2023-10-01 23:03:19 +0100

    added aboutme and some changes

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
+import About from "@/components/about/page";
 import Home from "@/components/home/page";
 import Navbar from "@/components/navbar/page";
 import { Separator } from "@/components/ui/separator"
+import Works from "@/components/works/page";
 import { useState } from "react";
 
 
 const MainPage = () => {
   return (
-<>
+  <>
+  <div className="bg-[#F9F9F9]  ">
 <Navbar />
 <Home />
+<About />
+</div>
 </>
-
-
   )
 }
 
---
+++ components/about/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import img1 from './../../assets/Curve_3x.png'
+/* eslint-disable react/no-unescaped-entities */
+
+
+const About = () => {
+  return (
+    <div >
+        <section className="flex flex-col bg-[#313338] w-full h-[715px] justify-center  pb-2 p-20 ">
+            <h1 className="text-6xl text-white font-sans font-extrabold -mt-[35px] ml-[7px] w-[360px] bg-[#313338] ">WHO IM I. ?</h1>
+            <p className="text-lg font-sans font-medium space-x-10 space-y-10 whitespace-normal text-zinc-100 p-4">Hey there, I'm Houssam, your friendly neighborhood web wizard hailing from the vibrant landscapes of Algeria. I'm a student by day, but when the sun sets, I transform into a coding maestro, graphic guru, and 3D sorcerer. <br />My digital adventures have taken me to the heart of web development, where I craft digital symphonies with lines of code, orchestrating seamless user experiences. But it doesn't stop there – I sprinkle a dash of artistic magic into everything I create. Graphic design and 3D modeling are my canvas, where I turn ideas into visual masterpieces. <br /> Beyond the screen, I'm a seeker of inspiration in every corner of Algeria. The rich tapestry of culture and nature here infuses my work with authenticity and creativity. <br /> In this ever-evolving digital universe, I'm committed to the never-ending quest for knowledge. Staying ahead of the curve is my game, and I'm always up for the challenge.<br />Explore my digital playground in my portfolio, where innovation meets imagination. If you've got a dream project or just want to geek out about the latest tech trends, let's connect. Together, we can turn your digital dreams into reality.</p>        
+        </section>
+    </div>
+  )
+}
+
+export default About
